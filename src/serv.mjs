e02d81f99f591764899f21520b5ddf7084754ccb
Sravan005 <138188417+Sravan005@users.noreply.github.com>
2024-03-05 12:00:15 +0530

    21 commit

--- src/serv.mjs
+++ src/serv.mjs
@@ -27,39 +27,28 @@ export const updateSSHConfig = async (newConfig) => {
 export const rhsaNumbers = [];
 
 
-
 export function initiateSSHConnection() {
-
   console.log('Connected via SSH');
 
   if (sshConfig.selectedButton === 'RHEL') {
-    exec(`ansible-playbook -e "ipadr=${sshConfig.host}" -e "username=${sshConfig.username}" -e "password=${sshConfig.password}"  ~/RHELapp/testv1.yml`, function (err, stream) {
+    const childProcess = exec(`ansible-playbook -e "ipadr=${sshConfig.host}" -e "username=${sshConfig.username}" -e "password=${sshConfig.password}"  ~/RHELapp/testv1.yml`, function (err, stdout, stderr) {
       if (err) throw err;
 
+      console.log('STDOUT:', stdout);
+      console.log('STDERR:', stderr);
 
-      stream.on('stderr', function (data) {
-        console.error('STDERR: ' + data);
+      const lines = stdout.toString().split('\n');
+      lines.forEach(line => {
+        const match = line.match(/RHSA-\d+:\d+/);
+        if (match) {
+          rhsaNumbers.push(match[0]); // Extract and store RHSA numbers
+        }
       });
+    });
 
-
-      stream.on('close', function (code, signal) {
-        console.log('Stream :: close :: code: ' + code + ', signal: ' + signal);
-        console.log('Extracted RHSA Numbers:', rhsaNumbers); // Print extracted RHSA numbers
-
-      }).on('data', function (data) {
-        console.log('STDOUT: ansible  ' + data);
-        const lines = data.toString().split('\n');
-        lines.forEach(line => {
-          const match = line.match(/RHSA-\d+:\d+/);
-          if (match) {
-            rhsaNumbers.push(match[0]); // Extract and store RHSA numbers
-          }
-        });
-      }).stderr.on('data', function (data) {
-        console.log('STDERR: ' + data);
-      });
+    childProcess.on('close', function (code, signal) {
+      console.log('Stream :: close :: code: ' + code + ', signal: ' + signal);
+      console.log('Extracted RHSA Numbers:', rhsaNumbers);
     });
   }
 }
-
-
