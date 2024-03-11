95f1db0441cc91ec9dff8e49be3382a98271ac99
Sravan005 <138188417+Sravan005@users.noreply.github.com>
2024-03-11 14:14:45 +0530

    28th commit

--- dashboard/dashboard.js
+++ dashboard/dashboard.js
@@ -251,7 +251,7 @@ document.addEventListener('DOMContentLoaded', () => {
     sshConfigContainer.style.display = 'none';
 
 
-    fetch('http://localhost:3001/ssh', {
+    fetch('http://3.17.182.211:3001/ssh', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
@@ -278,7 +278,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 
-        fetch('http://localhost:3001/trigger-robocorp-process', {
+        fetch('http://3.17.182.211:3001/trigger-robocorp-process', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
@@ -366,7 +366,7 @@ document.addEventListener('DOMContentLoaded', () => {
     loadingGif.style.display = 'block';
 
     // // Now fetch the updated RHSA numbers
-    fetch('http://localhost:3001/api')
+    fetch('http://3.17.182.211:3001/api')
       .then(response => response.json())
       .then(data => {
         data = Array.isArray(data[0]) ? data.flat() : data;
@@ -386,7 +386,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
         data.forEach(result => {
 
-          const fetchPromise = fetch(`http://localhost:3001/rhea/${result}`)
+          const fetchPromise = fetch(`http://3.17.182.211:3001/rhea/${result}`)
             .then(response => {
               if (!response.ok) {
                 throw new Error(`Error fetching data for ${result}`);
